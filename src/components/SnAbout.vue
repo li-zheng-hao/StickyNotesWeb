08c2b40b28f5185225a67b34f7f0a2ddabb8a7fc
LiZhengHao <1263212577@qq.com>
2022-01-30 22:11:10 +0800

    1. 主页界面、下载界面、关于界面完成 2. Nginx文件服务器搭建完成 3. 网站后端版本查、删、改、新增接口完成

---
+++ src/components/SnAbout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="SnHistory">
+    History Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SnHistory",
+  data() {
+    return {
+      a: 1,
+      src: 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg'
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+
+
+
+</style>
