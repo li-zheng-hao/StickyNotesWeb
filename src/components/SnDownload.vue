436299eb3c7674864d798d29f7cc257cb52f287d
LiZhengHao <1263212577@qq.com>
2022-02-04 13:17:37 +0800

    下载次数统计功能

--- src/components/SnDownload.vue
+++ src/components/SnDownload.vue
@@ -9,8 +9,10 @@
             <p v-for="history in item.history_text">
               {{history.history_text}}
             </p>
+            <p>下载次数: <el-tag size="medium">{{item.download_count}}</el-tag></p>
             <p class="p-author" >{{item.author}} 提交于 {{item.update_time}}</p>
-            <a :href="file_base+item.software_name+'_'+item.version+'.zip'"> <el-button type="primary" @click="clickDownload">点击下载</el-button>
+            <a :href="file_base+item.software_name+'_'+item.version+'.zip'">
+              <el-button type="primary" @click="clickDownload(item.software_name,item.version)">点击下载</el-button>
             </a>
           </el-card>
         </el-timeline-item>
@@ -31,7 +33,9 @@ export default {
     }
   },
   methods:{
-    clickDownload(){
+    async clickDownload(software_name,version){
+      var res=await  this.postRequest('/api/Software/DownloadCountAdd',{software_name,version});
+      console.log(res)
     }
   },
   async mounted() {
