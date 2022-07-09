df5324cfb85381235523bc2e12b922477dcf8ac9
LiZhengHao <1263212577@qq.com>
2022-07-09 22:04:24 +0800

    no message

--- src/components/SnDownload.vue
+++ src/components/SnDownload.vue
@@ -2,7 +2,7 @@
   <div class="sn-download">
     <div class="block">
       <el-timeline>
-        <el-timeline-item v-for="item in versionData" :timestamp="item.update_time" placement="top">
+        <el-timeline-item v-for="item in versionData" :key="item.version" :timestamp="item.update_time" placement="top">
           <el-card>
             <h4>版本: {{item.version}}</h4>
             <h5>更新内容:</h5>
@@ -11,7 +11,7 @@
             </p>
             <p>下载次数: <el-tag size="medium">{{item.download_count}}</el-tag></p>
             <p class="p-author" >{{item.author}} 提交于 {{item.update_time}}</p>
-            <a :href="file_base+item.software_name+'_'+item.version+'.zip'">
+            <a :href="item.download_url">
               <el-button type="primary" @click="clickDownload(item.software_name,item.version)">点击下载</el-button>
             </a>
           </el-card>
@@ -34,12 +34,12 @@ export default {
   methods:{
     async clickDownload(software_name,version){
       var res=await  this.postRequest('/api/Software/DownloadCountAdd',{software_name,version});
-      console.log(res)
+      console.log(res);
     }
   },
   async mounted() {
     let data=await  this.getRequest('/api/Software/QuerySoftwareData')
-    console.log(data.data);
+    console.log(data);
     this.versionData=data.data;
   }
 }
